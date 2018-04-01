83813834612f933ad36568e64d5ee963ee2c344f
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2018-03-31 21:14:10 -0600

    Added base classe to AST

--- ast.h
+++ ast.h
@@ -1,9 +1,82 @@
 #ifndef AST_H
 #define AST_H
 
-class Expression{
-public:
+enum StatemetTypes{
+  WhileStm,
+  ForStm,
+  IfStm,
+  AssiStm,
+  VarDeclStm,
+  ArrDeclStm,
+  FuncDeclStm,
+  ExprStm,
+  PrintStm,
 
+  StmListStm,
+};
+
+enum ExpressionTypes{
+  NumExp,
+  IdExp,
+  ArrAccExp,
+  FCallExp,
+  BoolExp,
+
+  DivExp,
+  ModExp,
+  MulExp,
+  LorExp,
+  LndExp,
+
+  AddExp,
+  SubExp,
+  ShlExp,
+  ShrExp,
+
+  PowExp,
+  NotExp,
+  BitNotExp,
+  StrExp,
+
+  LthExp,
+  GthExp,
+  LeqExp,
+  GeqExp,
+  NeqExp,
+  EquExp
+};
+
+class Expression {
+public:
+  virtual int getType() = 0;
+};
+
+class ExpressionList {
+public:
+  ExpressionList(){ }
+  void AddNew(Expression* exp){
+    exps.push_front(exp);
+  }
+
+  std::list<Expression*> exps;
+};
+
+class Statement {
+public:
+  virtual int getType() = 0;
+};
+
+class StatementList : public Statement{
+public:
+  StatementList(){ }
+
+  void AddNew(Statement* stm){
+    stms.push_front(stm);
+  }
+
+  int getType(){ return StmListStm; }
+
+  std::list<Statement*> stms;
 };
 
 #endif
