ddeaf47b686bc2ff0e130f596e7bef3afd0191f1
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2018-04-03 20:04:52 -0600

    Added return statement to expressions' GetAsm method in order to prevent 'double free' error

--- expressions.cpp
+++ expressions.cpp
@@ -18,19 +18,27 @@ AsmCode NumExpression::GetAsm(Scope* scope){
 }
 
 AsmCode BoolExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode IdExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode ArrayAccessExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode FuncCallExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode StrExpression::GetAsm(Scope* scope){
@@ -45,85 +53,127 @@ AsmCode StrExpression::GetAsm(Scope* scope){
 }
 
 AsmCode BitNotExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode LogNotExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode AddExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode SubExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode ShiftLeftExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode ShiftRightExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode BorExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode XorExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode DivExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode ModExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode MulExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode BandExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode PowExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode EquExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode NequExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode LthanExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode GthanExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode LeqExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode GeqExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode LorExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
 
 AsmCode LandExpression::GetAsm(Scope* scope){
+  AsmCode asmCode;
 
+  return asmCode;
 }
